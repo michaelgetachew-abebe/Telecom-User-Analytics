7322e86a4febd93606942f86d8f99249a29d2c53
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-27 13:49:30 +0300

    Feat(conn): connected all the pages in the dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,15 +3,13 @@ import streamlit as st
 # Custom imports 
 from multiapp import MultiPage
 #import the our pages
-from apps import user_overview, user_engagement, user_experience, user_satisfaction, ml_model
+from apps import home, user_overview, user_engagement, user_experience, user_satisfaction, ml_model
 
 # Create an instance of the app 
 app = MultiPage()
 
-# Title of the main page
-st.title("TELECOM USER ANALYTICS IN CASE OF: TellCo COMAPNY")
-
 # Add all your applications (pages) here
+app.add_page("HOME", home.app)
 app.add_page("USER OVERVIEW", user_overview.app)
 app.add_page("USER ENGAGEMENT ANALYSIS", user_engagement.app)
 app.add_page("USER EXPERIENCE ANALYSIS", user_experience.app)
--- dashboard/apps/user_overview.py
+++ dashboard/apps/user_overview.py
@@ -1,11 +1,22 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join('../../scripts')))
+sys.path.append(os.path.abspath(os.path.join('../../data')))
 import streamlit as st
 import pandas as pd
 
+def app():
+    # Loading the various dataframes needed!!!
+    original_df = pd.read_csv('../../data/Week1_challenge_data_source(CSV).csv')
+    #clean_df = pd.read_csv('../../data/clened_data.csv')
 
-st.title("USER OVERVIEW PAGE")
+    st.title("USER OVERVIEW PAGE")
 
-st.subheader("Data Introduction")
-st.markdown("This section gives an introduction to the telecommunication dataset which is about the customers of the TellCo company, in Republic of Pefkakia. In this section we will get a birds eyeview statistical decription of the dataset.")
+    st.subheader("Data Introduction")
+    st.markdown("This section gives an introduction to the telecommunication dataset which is about the customers of the TellCo company, in Republic of Pefkakia. In this section we will get a birds eyeview statistical decription of the dataset.")
+    st.write(original_df)
 
-st.subheader("INSIGHTS ABOUT CUSTOMERS")
-st.markdown("This section gives insights about customers by aggreagting them into different categories in the dataset.")
+
+    st.subheader("INSIGHTS ABOUT CUSTOMERS")
+    st.markdown("This section gives insights about customers by aggreagting them into different categories in the dataset.")
+    #st.write(original_df)
