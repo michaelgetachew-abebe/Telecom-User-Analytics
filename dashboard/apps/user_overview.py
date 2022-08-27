bf2a52791ee42c6c8c33dbac2c64aa0dba4274e1
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-27 11:24:50 +0300

    Feat(Add): added some code base to the user overview pages

---
+++ dashboard/apps/user_overview.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("USER OVERVIEW PAGE")
+
+st.subheader("Data Introduction")
+st.markdown("This section gives an introduction to the telecommunication dataset which is about the customers of the TellCo company, in Republic of Pefkakia. In this section we will get a birds eyeview statistical decription of the dataset.")
+
+st.subheader("INSIGHTS ABOUT CUSTOMERS")
+st.markdown("This section gives insights about customers by aggreagting them into different categories in the dataset.")
